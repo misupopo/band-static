2a771189e7c17228329b5c0f3ce5f9d62503890a
misupopo <kokorononakaniitumo@yahoo.co.jp>
2018-02-20 18:47:02 +0900

    config.jsonファイルの作成

--- src/app/@core/data/request.service.ts
+++ src/app/@core/data/request.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
+import { accessUrl } from '../../../assets/config/config.json';
 
 @Injectable()
 export class RequestConfigService {
-
     private data = {
-        url: 'http://localhost:9001',
+        url: '',
     };
 
     constructor() {
+        this.data.url = accessUrl;
     }
 
     public getRequestUrl (): string {
